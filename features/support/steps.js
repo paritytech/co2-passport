7609a5b18ba72c571a3c9109307cb7ef6cb4b17f
Daniel Bigos <daniel.bigos@parity.io>
2023-05-22 16:16:10 +0200

    chore: add JS formatters

--- features/support/steps.js
+++ features/support/steps.js
@@ -3,85 +3,125 @@ const { expect } = require("chai");
 
 /* User Story 1 (us1) */
 
-Given('I have the environment prepared.', async function () {
-  await this.prepareEnvironment();
+Given("I have the environment prepared.", async function () {
+	await this.prepareEnvironment();
 });
 
-When('{string} creates an asset with metadata: {string} and {string} emissions with the amount: {int} Grams per kilo CO2 emitted from date: {int}.', async function (caller, metadata, emission_category, emissions, date) {
-  let emissionInfo = {
-    "category": emission_category,
-    "primary": true,
-    "balanced": true,
-    "date": date,
-    "emissions": emissions
-  }
-  let assetParent = null
-  await this.blastAsset(caller, metadata, assetParent, emissionInfo);
+When(
+	"{string} creates an asset with metadata: {string} and {string} emissions with the amount: {int} Grams per kilo CO2 emitted from date: {int}.",
+	async function (caller, metadata, emission_category, emissions, date) {
+		let emissionInfo = {
+			category: emission_category,
+			primary: true,
+			balanced: true,
+			date: date,
+			emissions: emissions,
+		};
+		let assetParent = null;
+		await this.blastAsset(caller, metadata, assetParent, emissionInfo);
+	}
+);
+
+Then("the following events will be emitted:", function (jsonString) {
+	let events = JSON.parse(jsonString);
+	expect(this.events).to.deep.equal(events);
 });
 
-Then('the following events will be emitted:', function (jsonString) {
-  let events = JSON.parse(jsonString);
-  expect(this.events).to.deep.equal(events);
+Given(
+	"the {string} has blasted the asset with the following parameters:",
+	async function (caller, jsonString) {
+		let asset = JSON.parse(jsonString);
+
+		let emissionInfo = {
+			category: asset.emission_category,
+			primary: true,
+			balanced: true,
+			date: asset.date,
+			emissions: asset.emissions,
+		};
+		let assetParent = null;
+
+		await this.prepareEnvironment();
+		await this.blastAsset(
+			caller,
+			asset.metadata,
+			assetParent,
+			emissionInfo
+		);
+	}
+);
+
+When(
+	"{string} transfers asset with ID {int} to {string} with new {string} emission with the amount of {int} grams per kilo on the date {int}",
+	async function (
+		seller,
+		assetId,
+		buyer,
+		emission_category,
+		emissions,
+		date
+	) {
+		await this.transferAsset(
+			seller,
+			assetId,
+			buyer,
+			emission_category,
+			emissions,
+			date
+		);
+	}
+);
+
+Then("the following transfer events will be emitted:", function (jsonString) {
+	let events = JSON.parse(jsonString);
+	expect(this.events).to.deep.equal(events);
 });
-
-Given('the {string} has blasted the asset with the following parameters:', async function (caller, jsonString) {
-  let asset = JSON.parse(jsonString);
-
-  let emissionInfo = {
-    "category": asset.emission_category,
-    "primary": true,
-    "balanced": true,
-    "date": asset.date,
-    "emissions": asset.emissions
-  }
-  let assetParent = null
-
-  await this.prepareEnvironment();
-  await this.blastAsset(caller, asset.metadata, assetParent, emissionInfo);
-
-});
-
-When('{string} transfers asset with ID {int} to {string} with new {string} emission with the amount of {int} grams per kilo on the date {int}',
-  async function (seller, assetId, buyer, emission_category, emissions, date) {
-    await this.transferAsset(seller, assetId, buyer, emission_category, emissions, date);
-  });
-
-Then('the following transfer events will be emitted:', function (jsonString) {
-  let events = JSON.parse(jsonString);
-  expect(this.events).to.deep.equal(events);
-});
-
 
 /* User Story 2 (us2) */
 
-Given('A {string} creates an asset that is split into child assets where the asset is defined as:', async function (caller, jsonString) {
-  let asset = JSON.parse(jsonString);
+Given(
+	"A {string} creates an asset that is split into child assets where the asset is defined as:",
+	async function (caller, jsonString) {
+		let asset = JSON.parse(jsonString);
 
-  let emissionInfo = {
-    "category": asset.emission_category,
-    "primary": true,
-    "balanced": true,
-    "date": asset.date,
-    "emissions": asset.emissions
-  }
-  let assetParent = [1, 5];
-  let assetParent2 = [2, 3];
+		let emissionInfo = {
+			category: asset.emission_category,
+			primary: true,
+			balanced: true,
+			date: asset.date,
+			emissions: asset.emissions,
+		};
+		let assetParent = [1, 5];
+		let assetParent2 = [2, 3];
 
-  await this.prepareEnvironment();
-  await this.blastAsset(caller, asset.metadata, null, emissionInfo);
-  await this.pauseAsset(caller, 1);
-  emissionInfo.emissions = 5;
-  await this.blastAsset(caller, asset.metadata, assetParent, emissionInfo);
-  await this.pauseAsset(caller, 2);
-  emissionInfo.emissions = 3;
-  await this.blastAsset(caller, asset.metadata, assetParent2, emissionInfo);
+		await this.prepareEnvironment();
+		await this.blastAsset(caller, asset.metadata, null, emissionInfo);
+		await this.pauseAsset(caller, 1);
+		emissionInfo.emissions = 5;
+		await this.blastAsset(
+			caller,
+			asset.metadata,
+			assetParent,
+			emissionInfo
+		);
+		await this.pauseAsset(caller, 2);
+		emissionInfo.emissions = 3;
+		await this.blastAsset(
+			caller,
+			asset.metadata,
+			assetParent2,
+			emissionInfo
+		);
+	}
+);
 
-});
+When(
+	"{string} performs a query on the asset with ID {int}",
+	async function (caller, assetId) {
+		await this.queryEmissions(caller, assetId);
+	}
+);
 
-When('{string} performs a query on the asset with ID {int}', async function (caller, assetId) {
-  await this.queryEmissions(caller, assetId);
-});
-
-Then('the following result should be returned', function (docString) {
-  expect(this.readOutput).to.deep.equal(JSON.parse(docString));
+Then("the following result should be returned", function (docString) {
+	expect(this.readOutput).to.deep.equal(JSON.parse(docString));
 });
--- features/support/world.js
+++ features/support/world.js
@@ -1,208 +1,239 @@
-const fs = require('fs');
+const fs = require("fs");
 const { setWorldConstructor } = require("cucumber");
 
 const { CodePromise, ContractPromise } = require("@polkadot/api-contract");
 const { ApiPromise, WsProvider, Keyring } = require("@polkadot/api");
-const { mnemonicGenerate } = require("@polkadot/util-crypto")
-const { BN, BN_ONE } = require("@polkadot/util");
+const { BN } = require("@polkadot/util");
 
 const contractAbi = require("../../target/ink/asset_co2_emissions.json");
-const contract = JSON.parse(fs.readFileSync("./target/ink/asset_co2_emissions.contract"));
+const contract = JSON.parse(
+	fs.readFileSync("./target/ink/asset_co2_emissions.contract")
+);
 
 const REF_TIME = new BN(300_000_000_000);
 const PROOF_SIZE = new BN(1000000);
 
 // dev mnemonic
-const MNENOMIC = 'bottom drive obey lake curtain smoke basket hold race lonely fit walk';
+const MNENOMIC =
+	"bottom drive obey lake curtain smoke basket hold race lonely fit walk";
 
 class UserStoryWorld {
-    constructor() {
-        this.api = null;
-        this.contract = null;
-        this.sudo = null;
-        this.result = '';
-        this.events = [];
-        this.readOutput = null;
-        this.accounts = {};
-        this.keyring = new Keyring({ type: 'sr25519' });
-        this.sendTxOptions = null;
-    }
+	constructor() {
+		this.api = null;
+		this.contract = null;
+		this.sudo = null;
+		this.result = "";
+		this.events = [];
+		this.readOutput = null;
+		this.accounts = {};
+		this.keyring = new Keyring({ type: "sr25519" });
+		this.sendTxOptions = null;
+	}
 
-    async prepareEnvironment() {
-        this.sudo = this.keyring.addFromUri("//Alice");
+	async prepareEnvironment() {
+		this.sudo = this.keyring.addFromUri("//Alice");
 
-        const wsProvider = new WsProvider("ws://127.0.0.1:9944");
-        this.api = await ApiPromise.create({ provider: wsProvider });
+		const wsProvider = new WsProvider("ws://127.0.0.1:9944");
+		this.api = await ApiPromise.create({ provider: wsProvider });
 
-        const storageDepositLimit = 1_000_000_000_000;
-        this.sendTxOptions = {
-            gasLimit: this.api.registry.createType("WeightV2", {
-                refTime: REF_TIME,
-                proofSize: PROOF_SIZE
-            }),
-            storageDepositLimit
-        };
+		const storageDepositLimit = 1_000_000_000_000;
+		this.sendTxOptions = {
+			gasLimit: this.api.registry.createType("WeightV2", {
+				refTime: REF_TIME,
+				proofSize: PROOF_SIZE,
+			}),
+			storageDepositLimit,
+		};
 
-        await this.deploySmartContract();
-        await this.initiateAccountWithBalance("Seller", 100_000_000_000_000);
-        await this.initiateAccountWithBalance("Buyer", 100_000_000_000_000);
-        await this.initiateAccountWithBalance("Eve", 100_000_000_000_000);
-    }
+		await this.deploySmartContract();
+		await this.initiateAccountWithBalance("Seller", 100_000_000_000_000);
+		await this.initiateAccountWithBalance("Buyer", 100_000_000_000_000);
+		await this.initiateAccountWithBalance("Eve", 100_000_000_000_000);
+	}
 
-    async deploySmartContract() {
-        const code = new CodePromise(this.api, contractAbi, contract.source.wasm);
+	async deploySmartContract() {
+		const code = new CodePromise(
+			this.api,
+			contractAbi,
+			contract.source.wasm
+		);
 
-        const tx = code.tx.new(this.sendTxOptions);
+		const tx = code.tx.new(this.sendTxOptions);
 
-        // Wait for the smart contract to deploy, and contract address set
-        await new Promise((resolve) => {
-            this.signAndSend(this.sudo, tx, result => {
-                this.setContractAddress(result)
-                resolve();
-            })
-        });
-    }
+		// Wait for the smart contract to deploy, and contract address set
+		await new Promise((resolve) => {
+			this.signAndSend(this.sudo, tx, (result) => {
+				this.setContractAddress(result);
+				resolve();
+			});
+		});
+	}
 
-    async blastAsset(account_name, metadata, assetParent, emissionInfo) {
-        const sender = this.accounts[account_name];
+	async blastAsset(account_name, metadata, assetParent, emissionInfo) {
+		const sender = this.accounts[account_name];
 
-        const assetOwner = sender.address;
-        const assetEmissions = [
-            {
-                "category": emissionInfo.category,
-                "primary": emissionInfo.primary,
-                "balanced": emissionInfo.balanced,
-                "date": emissionInfo.date,
-                "emissions": emissionInfo.emissions
-            }
-        ];
+		const assetOwner = sender.address;
+		const assetEmissions = [
+			{
+				category: emissionInfo.category,
+				primary: emissionInfo.primary,
+				balanced: emissionInfo.balanced,
+				date: emissionInfo.date,
+				emissions: emissionInfo.emissions,
+			},
+		];
 
-        let blastExtrinsic = this.contract.tx["assetCO2Emissions::blast"](
-            this.sendTxOptions,
-            assetOwner, metadata, assetEmissions, assetParent
-        );
+		let blastExtrinsic = this.contract.tx["assetCO2Emissions::blast"](
+			this.sendTxOptions,
+			assetOwner,
+			metadata,
+			assetEmissions,
+			assetParent
+		);
 
+		await new Promise((resolve) => {
+			this.signAndSend(sender, blastExtrinsic, (result) => {
+				this.events = this.getEvents(result);
+				resolve();
+			});
+		});
+	}
 
-        await new Promise((resolve) => {
-            this.signAndSend(sender, blastExtrinsic, result => {
-                this.events = this.getEvents(result)
-                resolve();
-            });
-        });
-    }
+	async transferAsset(
+		senderName,
+		assetId,
+		recipientName,
+		emissionCategory,
+		emissions,
+		date
+	) {
+		const sender = this.accounts[senderName];
+		const receiver = this.accounts[recipientName];
 
-    async transferAsset(senderName, assetId, recipientName, emissionCategory, emissions, date) {
-        const sender = this.accounts[senderName];
-        const receiver = this.accounts[recipientName];
+		const assetEmissions = [
+			{
+				category: emissionCategory,
+				primary: true,
+				balanced: true,
+				date: date,
+				emissions: emissions,
+			},
+		];
 
-        const assetEmissions = [
-            {
-                "category": emissionCategory,
-                "primary": true,
-                "balanced": true,
-                "date": date,
-                "emissions": emissions
-            }
-        ];
+		let transferExtrinsic = this.contract.tx["assetCO2Emissions::transfer"](
+			this.sendTxOptions,
+			receiver.address,
+			assetId,
+			assetEmissions
+		);
 
-        let transferExtrinsic = this.contract.tx["assetCO2Emissions::transfer"](
-            this.sendTxOptions,
-            receiver.address, assetId, assetEmissions
-        );
+		await new Promise((resolve) => {
+			this.signAndSend(sender, transferExtrinsic, (result) => {
+				this.events = this.getEvents(result);
+				resolve();
+			});
+		});
+	}
 
+	async pauseAsset(senderName, assetId) {
+		const sender = this.accounts[senderName];
 
-        await new Promise((resolve) => {
-            this.signAndSend(sender, transferExtrinsic, result => {
-                this.events = this.getEvents(result)
-                resolve();
-            });
-        });
-    }
+		let pauseExtrinsic = this.contract.tx["assetCO2Emissions::pause"](
+			this.sendTxOptions,
+			assetId
+		);
 
-    async pauseAsset(senderName, assetId) {
-        const sender = this.accounts[senderName];
+		await new Promise((resolve) => {
+			this.signAndSend(sender, pauseExtrinsic, (result) => {
+				this.events = this.getEvents(result);
+				resolve();
+			});
+		});
+	}
 
-        let pauseExtrinsic = this.contract.tx["assetCO2Emissions::pause"](
-            this.sendTxOptions,
-            assetId
-        );
+	async queryEmissions(senderName, assetId) {
+		const sender = this.accounts[senderName];
 
+		const { result, output } = await this.contract.query[
+			"assetCO2Emissions::queryEmissions"
+		](sender.address, this.sendTxOptions, assetId);
 
-        await new Promise((resolve) => {
-            this.signAndSend(sender, pauseExtrinsic, result => {
-                this.events = this.getEvents(result)
-                resolve();
-            });
-        });
-    }
+		if (result.isOk) {
+			this.readOutput = output.toJSON().ok;
+		} else {
+			let registryErr = this.api.registry.findMetaError(
+				result.dispatchError.asModule
+			);
+			throw new Error(
+				`Tx failed with error: ${JSON.stringify(registryErr, null, 2)}`
+			);
+		}
+	}
 
-    async queryEmissions(senderName, assetId) {
-        const sender = this.accounts[senderName];
+	async initiateAccountWithBalance(accountName, balance) {
+		const account = this.keyring.addFromUri(MNENOMIC + "//" + accountName);
+		this.accounts[accountName] = account;
 
-        const { result, output } = await this.contract.query["assetCO2Emissions::queryEmissions"](
-            sender.address,
-            this.sendTxOptions,
-            assetId,
-        );
+		const extrinsic = this.api.tx.sudo.sudo(
+			this.api.tx.balances.setBalance(account.address, balance, 0)
+		);
 
-        if (result.isOk) {
-            this.readOutput = output.toJSON().ok;
-        }else{
-            let registryErr = this.api.registry.findMetaError(result.dispatchError.asModule);
-            throw new Error(`Tx failed with error: ${JSON.stringify(registryErr, null, 2)}`);
-        }
-    }
+		await this.signAndSend(this.sudo, extrinsic, this.doNothing);
+	}
 
-    async initiateAccountWithBalance(accountName, balance) {
-        const account = this.keyring.addFromUri(MNENOMIC + "//" + accountName);
-        this.accounts[accountName] = account;
+	// eslint-disable-next-line  no-unused-vars
+	doNothing(result) {
+		//    console.log(result)
+	}
 
-        const extrinsic = this.api.tx.sudo.sudo(this.api.tx.balances.setBalance(account.address, balance, 0));
+	getEvents(result) {
+		let events = [];
+		for (const event of result.contractEvents) {
+			const eventItem = {
+				event: {
+					name: event.event.identifier,
+					args: event.args.map((v) => v.toHuman()),
+				},
+			};
+			events.push(eventItem);
+		}
+		return events;
+	}
 
-        await this.signAndSend(this.sudo, extrinsic, this.doNothing);
-    }
+	setContractAddress({ contract }) {
+		const contractAddress = contract.address.toString();
 
-    doNothing(result) {
-        //    console.log(result)
+		// Proper way of creating Contract JS Object
+		this.contract = new ContractPromise(
+			this.api,
+			contractAbi,
+			contractAddress
+		);
+	}
 
-    }
+	async signAndSend(wallet, extrinsic, callback) {
+		await extrinsic.signAndSend(wallet, (result) => {
+			const status = result.status;
+			const dispatchError = result.dispatchError;
 
-    getEvents(result) {
-        let events = [];
-        for (const event of result.contractEvents) {
-            const eventItem = {
-                event: {
-                    name: event.event.identifier,
-                    args: event.args.map((v) => v.toHuman()),
-                },
-            };
-            events.push(eventItem);
-        }
-        return events;
-    }
-
-    setContractAddress({ contract }) {
-        const contractAddress = contract.address.toString();
-
-        // Proper way of creating Contract JS Object
-        this.contract = new ContractPromise(this.api, contractAbi, contractAddress);
-    }
-
-    async signAndSend(wallet, extrinsic, callback) {
-        await extrinsic.signAndSend(wallet, (result) => {
-            const status = result.status;
-            const dispatchError = result.dispatchError;
-
-            if (status.isInBlock || status.isFinalized) {
-                if (dispatchError) {
-                    let registryErr = this.api.registry.findMetaError(dispatchError.asModule);
-                    throw new Error(`Tx failed with error: ${JSON.stringify(registryErr, null, 2)}`);
-                } else {
-                    callback(result);
-                }
-            }
-        });
-    }
+			if (status.isInBlock || status.isFinalized) {
+				if (dispatchError) {
+					let registryErr = this.api.registry.findMetaError(
+						dispatchError.asModule
+					);
+					throw new Error(
+						`Tx failed with error: ${JSON.stringify(
+							registryErr,
+							null,
+							2
+						)}`
+					);
+				} else {
+					callback(result);
+				}
+			}
+		});
+	}
 }
 
 setWorldConstructor(UserStoryWorld);
