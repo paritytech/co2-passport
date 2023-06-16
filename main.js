7b4ee5ebae9d2a05b6cef29e8451def0a6f00de0
Daniel Bigos <daniel.bigos@parity.io>
2023-06-16 16:54:32 +0200

    feat: add script for calculating fees
    Requires:
    - Contract deployed on local chain
    - Contract address set in .env file
    - Asset with ID 1 paused & assigned to Bob
    - Asset with ID 2 assigned to Bob

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+require("dotenv").config();
+
+const { execSync } = require("child_process");
+
 const { ContractPromise } = require("@polkadot/api-contract");
 const { ApiPromise, WsProvider, Keyring } = require("@polkadot/api");
 const { BN } = require("@polkadot/util");
@@ -11,66 +15,128 @@ const PROOF_SIZE = new BN(1_000_000);
 // Unlimited storage deposit
 const STORAGE_DEPOSIT_LIMIT = null;
 
-async function dryRun(contract, sender, message, txOptions, ...params) {
+async function calculateFees(
+	info,
+	contract,
+	sender,
+	message,
+	txOptions,
+	...params
+) {
 	console.log(
 		"\n-------------------------------------------------------------------------------------------------"
 	);
+	console.log("Calculating fees for:", info);
 	console.log("Dry run for: { message:", message, " sender:", sender, "}");
 
-	const { gasRequired, storageDeposit, result, output } =
-		await contract.query[message](sender, txOptions, ...params);
+	const { gasRequired, storageDeposit, result } = await dryRun(
+		contract,
+		sender,
+		message,
+		txOptions,
+		...params
+	);
 
-	// the gas consumed for contract execution
-	console.log("Gas Required:", gasRequired.toJSON());
+	console.log("Gas Required:", gasRequired.toHuman());
+	console.log("Storage Deposit:", storageDeposit.toHuman());
 
-	console.log("Storage Deposit:", storageDeposit.toJSON());
-
-	// check if the call was successful
 	if (result.isOk) {
-		console.log("Success:", output.toJSON().ok);
-		await calculateFees(contract, sender, message, txOptions, ...params);
+		await doCalculateFees(contract, sender, message, txOptions, ...params);
 	} else {
 		console.error("Error:", result.asErr.toJSON());
 	}
 }
 
-async function calculateFees(contract, sender, message, txOptions, ...params) {
+// eslint-disable-next-line  no-unused-vars
+async function queryState(contract, sender, message, txOptions, ...params) {
+	console.log(
+		"\n-------------------------------------------------------------------------------------------------"
+	);
+	console.log(
+		"Querying state for: { message:",
+		message,
+		" sender:",
+		sender,
+		"}"
+	);
+
+	const { result, output } = await dryRun(
+		contract,
+		sender,
+		message,
+		txOptions,
+		...params
+	);
+	if (result.isOk) {
+		console.log("Output:", output.toJSON().ok);
+	} else {
+		console.error("Error:", result.asErr.toJSON());
+	}
+}
+
+async function dryRun(contract, sender, message, txOptions, ...params) {
+	return await contract.query[message](sender, txOptions, ...params);
+}
+
+async function callContract(
+	info,
+	contract,
+	sender,
+	message,
+	txOptions,
+	...params
+) {
+	console.log(
+		"\n-------------------------------------------------------------------------------------------------"
+	);
+	console.log("Calling contract:", info);
 	let extrinsic = contract.tx[message](txOptions, ...params);
-	extrinsic
-		.paymentInfo(sender)
-		.then((result) =>
-			console.log("Expected txn fee:", result.partialFee.toHuman())
-		);
+	await signAndSend(sender, extrinsic);
+}
+
+async function waitForBlock() {
+	console.log("Waiting for a block for 15 seconds...");
+	execSync("sleep 15");
+}
+
+async function signAndSend(wallet, extrinsic) {
+	await extrinsic.signAndSend(wallet, (result) => {
+		const status = result.status;
+		const dispatchError = result.dispatchError;
+
+		if (status.isInBlock || status.isFinalized) {
+			if (dispatchError) {
+				console.log("Contract Call error:", result.asErr.toJSON());
+			} else {
+				console.log("Contract Call successfull!");
+			}
+		}
+	});
+}
+
+async function doCalculateFees(
+	contract,
+	sender,
+	message,
+	txOptions,
+	...params
+) {
+	const extrinsic = contract.tx[message](txOptions, ...params);
+	const result = await extrinsic.paymentInfo(sender);
+	console.log("Expected txn fee:", result.partialFee.toHuman());
 }
 
 async function main() {
-	// Shibuya RPC
-    const wsProvider = new WsProvider("wss://rpc.shibuya.astar.network");
-
-	// Rococo RPC
-	// const wsProvider = new WsProvider("wss://rococo-contracts-rpc.polkadot.io");
-
-	// Local Node RPC
-	// const wsProvider = new WsProvider("ws://127.0.0.1:9944");
-
+	const endpoint = process.env.NODE_RPC;
+	const wsProvider = new WsProvider(endpoint);
 	const api = await ApiPromise.create({ provider: wsProvider });
-
-	// Shibuya Contract Address
-    const contractAddress = "Z9RhEdrpQVvfMRXVKaVb1tpxzZkKNBt5DeUxqgQ6BXtAvuY";
-
-	// Rococo Contract Address
-	// const contractAddress = "5HTY8YRLaLwhxzQPJACUiKYrceNCmrB1RgjPJgNDKn1qZp7b";
-
-	// TODO set Local Node Contract Address Here
-	// Local Node Contract Address
-	// const contractAddress = "5Cip81QsAXC4iyW6V5NwDFnBHij3e9d5DqTW6AnZzdC9iWZG";
-	// "5DG9oUoaWqnedg7dj964tVYHrUUsxL7C5nLoiUWTxSSitSig";
-
-	const contract = new ContractPromise(api, CONTRACT_ABI, contractAddress);
 
 	let keyring = new Keyring({ type: "sr25519" });
 	// eslint-disable-next-line  no-unused-vars
-	const alice = keyring.addFromUri("//Alice");
+	const account = keyring.addFromUri("//Bob");
+
+	const contractAddress = process.env.CONTRACT_ADDRESS;
+	const contract = new ContractPromise(api, CONTRACT_ABI, contractAddress);
 
 	const txOptions = {
 		gasLimit: api.registry.createType("WeightV2", {
@@ -80,43 +146,158 @@ async function main() {
 		STORAGE_DEPOSIT_LIMIT,
 	};
 
-	// Shibuya Account Address
-    const testingAddress = "aTpKaUyG6uFSNoctp96noX1WWQWsydJXf9p99VFaT26c9mW";
+	const testingAddress = process.env.TESTING_ADDRESS;
 
-	// Rococo Account Address
-	// const testingAddress = "5GbDtHWtUsG9DYmnQWgym9MjRThPukvZWTefuSHPi62927YS";
-
-	// Local Node Account Address
-	// const testingAddress = alice.address;
-
-	const message = "assetCO2Emissions::blast";
+	const blastMessage = "assetCO2Emissions::blast";
+	const transferMessage = "assetCO2Emissions::transfer";
+	const pauseMessage = "assetCO2Emissions::pause";
+	const addEmissionsMessage = "assetCO2Emissions::addEmissions";
 
 	const assetOwner = testingAddress;
 	const assetMetadata =
-		'{"weight": 1666, "batch_id": "CXW123ABCF", "item_num": "CXW123ABCF", "certified": true, "scrap_percentage": 75, "renewable_energy_mix": [{"mix": 30, "source": "hydro"}]}';
+		"1234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678";
+	const CO2EmissionDataSource =
+		"12345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678";
+	const CO2EmissionItem = {
+		category: "Process",
+		dataSource: CO2EmissionDataSource,
+		value: 1000,
+		balanced: true,
+		date: 1686559847,
+	};
 
-	const assetEmissions = [
-		{
-			category: "Process",
-			dataSource: "Nexigen",
-			value: 12,
-			balanced: true,
-			date: 1686559847,
-		},
-	];
+    const pausedAssetId = 1;
+    const assetId = 2;
 
-	const assetParent = null;
+	const parent = {
+		parentId: pausedAssetId,
+		relation: 1000,
+    };
 
-	await dryRun(
+	await calculateFees(
+		"Blast, single emission, no parent",
 		contract,
 		testingAddress,
-		message,
+		blastMessage,
 		txOptions,
 		assetOwner,
 		assetMetadata,
-		assetEmissions,
-		assetParent
+		[CO2EmissionItem],
+		null
 	);
+
+	await calculateFees(
+		"Blast, 2 emissions, no parent",
+		contract,
+		testingAddress,
+		blastMessage,
+		txOptions,
+		assetOwner,
+		assetMetadata,
+		[CO2EmissionItem, CO2EmissionItem],
+		null
+	);
+
+	await calculateFees(
+		"Blast, 3 emissions, no parent",
+		contract,
+		testingAddress,
+		blastMessage,
+		txOptions,
+		assetOwner,
+		assetMetadata,
+		[CO2EmissionItem, CO2EmissionItem, CO2EmissionItem],
+		null
+	);
+
+	await calculateFees(
+		"Blast, single emission, parent specified",
+		contract,
+		testingAddress,
+		blastMessage,
+		txOptions,
+		assetOwner,
+		assetMetadata,
+		[CO2EmissionItem],
+		parent
+	);
+
+	await calculateFees(
+		"Blast, 2 emissions, parent specified",
+		contract,
+		testingAddress,
+		blastMessage,
+		txOptions,
+		assetOwner,
+		assetMetadata,
+		[CO2EmissionItem, CO2EmissionItem],
+		parent
+	);
+
+	await calculateFees(
+		"Blast, 3 emissions, parent specified",
+		contract,
+		testingAddress,
+		blastMessage,
+		txOptions,
+		assetOwner,
+		assetMetadata,
+		[CO2EmissionItem, CO2EmissionItem, CO2EmissionItem],
+		parent
+	);
+
+
+	await calculateFees(
+		"Add Emissions",
+		contract,
+		testingAddress,
+		addEmissionsMessage,
+		txOptions,
+		assetId,
+		CO2EmissionItem
+	);
+
+	await calculateFees(
+		"Transfer, 1 emission",
+		contract,
+		testingAddress,
+		transferMessage,
+		txOptions,
+		"bgrbE6JxaHBk2tS7JH4rjcicyMki4t1E9M6YivFzCUq2YDg",
+		assetId,
+		[CO2EmissionItem]
+	);
+
+	await calculateFees(
+		"Transfer, 2 emissions",
+		contract,
+		testingAddress,
+		transferMessage,
+		txOptions,
+		"bgrbE6JxaHBk2tS7JH4rjcicyMki4t1E9M6YivFzCUq2YDg",
+		assetId,
+		[CO2EmissionItem, CO2EmissionItem]
+	);
+	await calculateFees(
+		"Transfer, 3 emission",
+		contract,
+		testingAddress,
+		transferMessage,
+		txOptions,
+		"bgrbE6JxaHBk2tS7JH4rjcicyMki4t1E9M6YivFzCUq2YDg",
+		assetId,
+		[CO2EmissionItem, CO2EmissionItem, CO2EmissionItem]
+	);
+
+	await calculateFees(
+		"Pause",
+		contract,
+		testingAddress,
+		pauseMessage,
+		txOptions,
+		assetId
+	);
+
 }
 
 main();
